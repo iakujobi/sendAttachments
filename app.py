70fc7f76d14a195195d9492e3fb1de653def692a
Ikechukwu Akujobi <iykejobi@gmail.com>
2022-01-27 15:27:29 -0500

    finish sendmail function

--- app.py
+++ app.py
@@ -21,15 +21,18 @@ def send_test_mail(body):
     msgText = MIMEText('<b>%s</b>' % (body), 'html')
     msg.attach(msgText)
 
-    filename = "example.txt"
+    # Text Attachment - CSV or TXT file
+    filename = "testFile.txt"
     msg.attach(MIMEText(open(filename).read()))
 
+    # Image Attachment - JPG or PNG file
     with open('example.jpg', 'rb') as fp:
         img = MIMEImage(fp.read())
         img.add_header('Content-Disposition', 'attachment',
                        filename="example.jpg")
         msg.attach(img)
 
+    # Binary Attachment - PDF or PPT file
     pdf = MIMEApplication(open("example.pdf", 'rb').read())
     pdf.add_header('Content-Disposition', 'attachment', filename="example.pdf")
     msg.attach(pdf)
@@ -44,8 +47,10 @@ def send_test_mail(body):
         print(e)
 
 
-@app.route('/')
+# Simple POST Route
+@app.route('/', methods=['POST'])
 def hello_world():
+    send_test_mail("Welcome to you Email Attachment!")
     return 'Hello, world!'
 
 
